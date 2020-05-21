edd70d88b077a9d5ce20f0784084fb59dc46ba81
Marc Suchard <msuchard@ucla.edu>
2020-05-21 15:59:24 -0700

    fix up shiny app

--- Covid19IncidenceRasInhibitors/inst/shiny/EvidenceExplorer/global.R
+++ Covid19IncidenceRasInhibitors/inst/shiny/EvidenceExplorer/global.R
@@ -48,4 +48,15 @@ for (removePart in removeParts) {
 
 tcos <- unique(cohortMethodResult[, c("targetId", "comparatorId", "outcomeId")])
 tcos <- tcos[tcos$outcomeId %in% outcomeOfInterest$outcomeId, ]
+
+## Added from JW
+
+outcomeOfInterest$definition <- NULL
+outcomeOfInterest <- outcomeOfInterest[!duplicated(outcomeOfInterest), ]
+
+exposureOfInterest$definition <- NULL
+exposureOfInterest <- exposureOfInterest[!duplicated(exposureOfInterest), ]
+
+cohortMethodAnalysis$definition <- NULL
+cohortMethodAnalysis <- cohortMethodAnalysis[!duplicated(cohortMethodAnalysis), ]
                
