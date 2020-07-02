589c492e79792667a8d0a5ac8aa9249f64e3aeed
Marc Suchard <msuchard@ucla.edu>
2020-07-02 13:46:41 -0700

    tweaks to shiny app names

--- Covid19ComplicationsRasInhibitors/R/ShinyApps.R
+++ Covid19ComplicationsRasInhibitors/R/ShinyApps.R
@@ -73,7 +73,7 @@ prepareForEvidenceExplorer <- function(resultsZipFile, dataFolder, prettyLabels 
 
   files <- list.files(tempFolder, ".*.csv")
   plyr::l_ply(files, processFile, .progress = "text")
-  
+
   if (prettyLabels) {
     # Relabel exposure_of_interest.csv
     exposureFile <- file.path(dataFolder, sprintf("exposure_of_interest_%s.rds", databaseId))
@@ -84,9 +84,23 @@ prepareForEvidenceExplorer <- function(resultsZipFile, dataFolder, prettyLabels 
     cohorts <- read.csv(cohortFile)[, c("cohortId", "shinyName", "shinyOrder")]
     cohorts$exposureId <- cohorts$cohortId
     colnames(cohorts) <- SqlRender::camelCaseToSnakeCase(colnames(cohorts))
-    tmp <- exposureOfInterest %>% dplyr::left_join(cohorts, by = "exposure_id") %>% 
+    tmp <- exposureOfInterest %>% dplyr::left_join(cohorts, by = "exposure_id") %>%
       dplyr::arrange(shiny_order) %>% dplyr::mutate(exposure_name = shiny_name)
     saveRDS(tmp, exposureFile)
+
+    # Relabel outcome_of_interest.rds
+    outcomeFile <- file.path(dataFolder, sprintf("outcome_of_interest_%s.rds", databaseId))
+    outcomeOfInterest <- readRDS(file = outcomeFile)
+    cohortFile <- system.file("settings",
+                              "CohortsToCreate.csv",
+                              package = "Covid19ComplicationsRasInhibitors")
+    cohorts <- read.csv(cohortFile)[, c("cohortId", "shinyName", "shinyOrder")]
+    cohorts$outcomeId <- cohorts$cohortId
+    colnames(cohorts) <- SqlRender::camelCaseToSnakeCase(colnames(cohorts))
+    tmp <- outcomeOfInterest %>% dplyr::left_join(cohorts, by = "outcome_id") %>%
+      dplyr::arrange(shiny_order) %>% dplyr::mutate(outcome_name = shiny_name) %>%
+      select(-shiny_name, -shiny_order)
+    saveRDS(tmp, outcomeFile)
   }
 }
 
